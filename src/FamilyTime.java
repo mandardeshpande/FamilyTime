65fef7df63c6b8898ec850f24372660273f62dc4
Deshpande, Mandar <Mandar_Deshpande@intuit.com>
2017-05-09 16:05:02 -0700

    After the interview

--- src/FamilyTime.java
+++ src/FamilyTime.java
@@ -1,4 +1,6 @@
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 /**
@@ -7,23 +9,32 @@ import java.util.*;
 public class FamilyTime {
 
     public List<String> employeeList = new ArrayList<>();
+    public HashMap<String, GroupInfo> groupInfoHashMap = new HashMap<>();
 
     public static void main(String args[]) {
 
         FamilyTime familyTime = new FamilyTime();
         List<String> employeeList = new ArrayList<>();
 
-        employeeList.add("Happy");
-        employeeList.add("Dopey");
-        employeeList.add("Grumpy");
-        employeeList.add("Sneezy");
-        employeeList.add("Bashful");
-        employeeList.add("Sleepy");
-        employeeList.add("Doc");
-        employeeList.add("Snow");
-        employeeList.add("AK");
-        employeeList.add("MD");
-        employeeList.add("SD");
+        int minmumPeople = Integer.parseInt(args[0]);
+        int maximumPeople = Integer.parseInt(args[1]);
+
+        int maxPeopleInGroup = maximumPeople;
+        int minPeopleInGroup = minmumPeople;
+
+        File dataFile = new File(args[2]);
+
+        try{
+
+            Scanner scan = new Scanner(dataFile);
+            while(scan.hasNextLine()){
+                employeeList.add(scan.nextLine());
+            }
+            scan.close();
+
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
 
         familyTime.employeeList = employeeList;
 
@@ -32,11 +43,11 @@ public class FamilyTime {
         System.out.println(Arrays.toString(employeeList.toArray()));
 
         int numberOfPeople = employeeList.size();
+
         List<List<String>> listOfGroups = new ArrayList<>();
         List<String> group = new ArrayList<>();
 
-        int maxPeopleInGroup = 5;
-        int minPeopleInGroup = 3;
+
 
         while (maxPeopleInGroup != minPeopleInGroup) {
             int numberOfGroups = (numberOfPeople % maxPeopleInGroup) % minPeopleInGroup;
@@ -66,7 +77,13 @@ public class FamilyTime {
         while (numberOfGroups != 0) {
             for (int i = start; i < end; i++) {
                 group.add(employeeList.get(i));
+                GroupInfo grpInfo = new GroupInfo();
                 if (group.size() == peopleInGroup) {
+
+                    grpInfo.peoplePresentInGroup = group;
+                    grpInfo.UUID = UUID.randomUUID().toString();
+                    groupInfoHashMap.getOrDefault( grpInfo.UUID,  grpInfo);
+
                     listOfGroups.add(group);
                     group = new ArrayList<>();
                     numberOfGroups--;
@@ -74,4 +91,9 @@ public class FamilyTime {
             }
         }
     }
+}
+
+class GroupInfo {
+    String UUID;
+    List<String> peoplePresentInGroup;
 }
